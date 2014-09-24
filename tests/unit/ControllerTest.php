350261a97d82a513fb74e6fc3bb5d252c258565a
Albert Ovide <albertovide@gmail.com>
2014-09-24 23:59:55 +0200

    Added empty ControllerTest

---
+++ tests/unit/ControllerTest.php
@@ -0,0 +1,27 @@
+<?php namespace Ovide\Libs\Mvc\Rest;
+
+require_once __DIR__.'/../mocks/BasicMock.php';
+require_once __DIR__.'/../mocks/FooMock.php';
+
+class ControllerTest extends \Codeception\TestCase\Test
+{
+   /**
+    * @var \UnitTester
+    */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+
+    }
+
+}
