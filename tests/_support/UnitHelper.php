467b7ebb691d0867f39a2e43c1d4e590c408d73a
Albert Ovide <albertovide@gmail.com>
2014-10-02 00:38:49 +0200

    Clean tests dir

--- tests/_support/UnitHelper.php
+++ tests/_support/UnitHelper.php
@@ -1,4 +1,8 @@
-<?php namespace Codeception\Module;
+<?php
+namespace Codeception\Module;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
 
 class UnitHelper extends \Codeception\Module
 {
